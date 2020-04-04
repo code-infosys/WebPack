1e5152ada7f46f9ed4b8bdfa513a84bc33103f98
davsin <davinder.aspx@gmail.com>
2020-04-04 15:11:57 +0530

    Add project files.

---
+++ WebPack.Helper/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebPack.Helper
+{
+    public class Helper
+    {
+        public string GetMessage(string inputMessage)
+        {
+            return "Your message is: " + inputMessage;
+        }
+
+        public int GetSum(int value1,int value2)
+        {
+            return (value1 + value2);
+        }
+    }
+}
